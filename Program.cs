e14125efb8db7bea35af409b1bbfbf20c77b3ee3
Emil <mawlitov.e@yandex.ru>
2022-11-30 19:50:27 +0400

    first2

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите превое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 > number2)
+{
+    Console.WriteLine("Первое число больше второго");
+}
+else if(number1 < number2)
+{
+    Console.WriteLine("Второе число больше первого");
+}
